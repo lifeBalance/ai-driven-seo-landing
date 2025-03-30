55812981caf5f7dcc104b8e923978b1036ff33b5
Javi <3051496+lifeBalance@users.noreply.github.com>
2025-03-30 14:03:49 +0300

    add animation to testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -2,6 +2,7 @@ import avatar1 from '@assets/avatar-1.png'
 import avatar2 from '@assets/avatar-2.png'
 import avatar3 from '@assets/avatar-3.png'
 import avatar4 from '@assets/avatar-4.png'
+import { motion } from 'framer-motion'
 
 export default function Testimonials() {
   return (
@@ -15,36 +16,47 @@ export default function Testimonials() {
           Our revolutionary AI SEO tools have transformed our clients strategies
         </p>
 
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map((testimonial: Testimonial, idx: number) => (
-              <div
-                key={`${testimonial.name}-${idx}`}
-                className="border border-white/15 rounded-xl p-6 md:p-10 bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-sm flex-none"
-              >
-                <div className="text-lg md:text-xl tracking-tight">
-                  "{testimonial.text}"
-                </div>
-
-                <div className="flex items-center gap-3 mt-5">
-                  <div className='relative after:content-[""] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[""] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg'>
-                    <img
-                      src={testimonial.avatar}
-                      alt={`Avatar for ${testimonial.name}`}
-                      className="h-11 w-11 rounded-lg grayscale"
-                    />
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{ translateX: '-50%' }}
+            animate={{ translateX: '0' }}
+            transition={{
+              repeat: Infinity,
+              ease: 'linear',
+              duration: 30,
+            }}
+            className="flex flex-none gap-5"
+          >
+            {[...testimonials, ...testimonials].map(
+              (testimonial: Testimonial, idx: number) => (
+                <div
+                  key={`${testimonial.name}-${idx}`}
+                  className="border border-white/15 rounded-xl p-6 md:p-10 bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-sm flex-none"
+                >
+                  <div className="text-lg md:text-xl tracking-tight">
+                    "{testimonial.text}"
                   </div>
 
-                  <div className="">
-                    <div>{testimonial.name}</div>
-                    <div className="text-white/50 text-sm">
-                      {testimonial.title}
+                  <div className="flex items-center gap-3 mt-5">
+                    <div className='relative after:content-[""] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[""] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg'>
+                      <img
+                        src={testimonial.avatar}
+                        alt={`Avatar for ${testimonial.name}`}
+                        className="h-11 w-11 rounded-lg grayscale"
+                      />
+                    </div>
+
+                    <div className="">
+                      <div>{testimonial.name}</div>
+                      <div className="text-white/50 text-sm">
+                        {testimonial.title}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ),
+            )}
+          </motion.div>
         </div>
       </div>
     </section>
