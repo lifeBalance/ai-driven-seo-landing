dd669925b8cfb0b83b3c38e87633e3fe5210d2ca
Javi <3051496+lifeBalance@users.noreply.github.com>
2025-03-30 12:52:45 +0300

    add border animation

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,8 +1,17 @@
 import { DotLottie, DotLottieReact } from '@lottiefiles/dotlottie-react'
 import productImage from '@assets/product-image.png'
-import { useState } from 'react'
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from 'react'
+import {
+  animate,
+  useMotionTemplate,
+  useMotionValue,
+  ValueAnimationTransition,
+} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 export default function Features() {
+  const [selectedTab, setSelectedTab] = useState(0)
+
   return (
     <section className="py-20 md:py-24">
       <div className="container">
@@ -17,7 +26,18 @@ export default function Features() {
 
         <div className="mt-10 flex flex-col lg:flex-row gap-3">
           {tabs.map((tab: Tab, idx: number) => (
-            <Tab key={`${idx}-${tab.title}`} tab={tab} />
+            <Tab
+              key={`${idx}-${tab.title}`}
+              {...tab}
+              // icon={tab.icon}
+              // title={tab.title}
+              // isNew={tab.isNew}
+              // backgroundPositionX={tab.backgroundPositionX}
+              // backgroundPositionY={tab.backgroundPositionY}
+              // backgroundSize={tab.backgroundSize}
+              selected={idx === selectedTab}
+              onClick={() => setSelectedTab(idx)}
+            />
           ))}
         </div>
 
@@ -34,8 +54,41 @@ export default function Features() {
   )
 }
 
-const Tab = ({ tab }: { tab: (typeof tabs)[number] }) => {
+const Tab = (
+  props: (typeof tabs)[number] &
+    ComponentPropsWithoutRef<'div'> & { selected: boolean },
+) => {
   const [dotLottie, setDotLottie] = useState<DotLottie | null>(null)
+  const tabRef = useRef<HTMLDivElement>(null)
+
+  // For the border animation
+  useEffect(() => {
+    if (!tabRef.current || !props.selected) return
+
+    // Reset the border animation
+    xPercentage.set(0)
+    yPercentage.set(0)
+
+    const { height, width } = tabRef.current.getBoundingClientRect()
+    const circumference = height * 2 + width * 2
+    // Times is a sequence of values between 0 and 1 that represent the percentage of the animation
+    const times = [
+      0,
+      width / circumference,
+      (width + height) / circumference,
+      (width * 2 + height) / circumference,
+      1,
+    ]
+    const options: ValueAnimationTransition = {
+      times,
+      duration: 5,
+      repeat: Infinity,
+      repeatType: 'loop',
+      ease: 'linear',
+    }
+    animate(xPercentage, [0, 100, 100, 0, 0], options)
+    animate(yPercentage, [0, 0, 100, 100, 0], options)
+  }, [props.selected])
 
   const dotLottieRefCallback: (
     dotLottie: DotLottie | null,
@@ -49,23 +102,37 @@ const Tab = ({ tab }: { tab: (typeof tabs)[number] }) => {
     }
   }
 
+  // Border animation
+  const xPercentage = useMotionValue(0)
+  const yPercentage = useMotionValue(0)
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%,black,transparent)`
+
   return (
     <div
+      ref={tabRef}
       onMouseEnter={play}
-      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1"
+      onClick={props.onClick}
+      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative"
     >
+      {props.selected && (
+        <motion.div
+          style={{ maskImage }}
+          className="absolute inset-0 border border-[#a369ff] -m-px rounded-xl"
+        ></motion.div>
+      )}
+
       <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
         <DotLottieReact
           dotLottieRefCallback={dotLottieRefCallback}
-          src={tab.icon}
+          src={props.icon}
           autoplay
           className="w-5 h-5"
         />
       </div>
 
-      <div>{tab.title}</div>
+      <div>{props.title}</div>
 
-      {tab.isNew && (
+      {props.isNew && (
         <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
           new
         </div>
